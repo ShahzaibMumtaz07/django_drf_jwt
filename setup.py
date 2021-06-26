8b2ea606a0fc63f54406ac8e5bcecd699625fe4f
ShahzaibMumtaz07 <shahzaib.mumtaz.20195@gmail.com>
2021-06-26 21:57:55 +0500

    updated setup

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='django-rest-jwt',
+    name='django_rest_jwt',
     version='0.0.1',
     description='A simple app to demonstrate django-jwt-authentication using Django REST Framework',
     url='git@github.com:ShahzaibMumtaz07/django-rest-jwt.git',
     author='Shahzaib Mumtaz',
     author_email='shahzaib.mumtaz.20195@gmail.com',
     zip_safe=False,
+    packages = ['django_rest_jwt']
     install_requires=[
         'Django',
         'djangorestframework',
